576650617d562fedcc2cf17ba89a50fd8ed2728e
Tien <annhgch18718@fpt.edu.vn>
2022-11-03 14:17:54 +0700

    doneuserapi

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,7 +1,6 @@
 import { NgModule } from "@angular/core";
 import { BrowserModule } from "@angular/platform-browser";
 
-<<<<<<< HEAD
 import { HttpClientModule } from "@angular/common/http";
 import { FormsModule } from "@angular/forms";
 
@@ -10,18 +9,6 @@ import { NavComponent } from "./components/nav/nav.component";
 import { FooterComponent } from "./components/footer/footer.component";
 import { MainComponent } from "./components/main/main.component";
 import { HomeComponent } from "./components/home/home.component";
-=======
-import { HttpClientModule } from '@angular/common/http';
-import { FormsModule } from '@angular/forms';
-
-import { AppComponent } from './app.component';
-import { LoginComponent } from './components/login/login.component';
-import { NavComponent } from './components/nav/nav.component';
-import { FooterComponent } from './components/footer/footer.component';
-import { MainComponent } from './components/main/main.component';
-import { HomeComponent } from './components/home/home.component';
-import { RouterModule } from '@angular/router';
->>>>>>> 10ced99575dfdc1797e8f6eca45507f0277456df
 
 @NgModule({
   declarations: [
@@ -30,30 +17,6 @@ import { RouterModule } from '@angular/router';
     FooterComponent,
     MainComponent,
     HomeComponent,
-<<<<<<< HEAD
-=======
-    LoginComponent,
-  ],
-  imports: [
-    BrowserModule,
-    FormsModule,
-    HttpClientModule,
-    RouterModule.forRoot([
-      {
-        path: '',
-        redirectTo: '/home',
-        pathMatch: 'full',
-      },
-      {
-        path: 'login',
-        component: LoginComponent,
-      },
-      {
-        path: 'home',
-        component: HomeComponent,
-      },
-    ]),
->>>>>>> 10ced99575dfdc1797e8f6eca45507f0277456df
   ],
   imports: [BrowserModule, FormsModule, HttpClientModule],
   providers: [],
--- src/app/services/product.service.ts
+++ src/app/services/product.service.ts
@@ -9,11 +9,7 @@ export class ProductService {
   private baseURL = "https://dummyjson.com";
 
   constructor(private http: HttpClient) {}
-<<<<<<< HEAD
   getAllProduct() {
-=======
-  getAllTodos() {
->>>>>>> 10ced99575dfdc1797e8f6eca45507f0277456df
     return this.http.get<pip>(this.baseURL + "/products");
   }
 }
